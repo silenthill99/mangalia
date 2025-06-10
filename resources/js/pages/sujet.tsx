5f8d5513c837534161e6589bcc7e0aecf9f4401e
silenthill99 <florian.graziani@sfr.fr>
2025-06-10 11:54:19 +0200

    Mise à jour du projet

--- resources/js/pages/sujet.tsx
+++ resources/js/pages/sujet.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Head, Link, usePage } from '@inertiajs/react';
+import { Button } from '@/components/ui/button';
+import { SharedData } from '@/types';
 
 type ArticleProps = {
     id: number;
@@ -12,7 +14,23 @@ type ArticleProps = {
 
 const Sujet = () => {
 
-    const  { article, menu } = usePage<{article : ArticleProps, menu: ArticleProps[]}>().props;
+    const  { auth, article, menu } = usePage<SharedData & {article : ArticleProps, menu: ArticleProps[]}>().props;
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    const menuRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+                setShowMenu(false)
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside)
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, []);
 
     return (
         <div className={"grid lg:grid-cols-[25%_50%_25%]"}>
@@ -29,9 +47,31 @@ const Sujet = () => {
                         ))}
                     </ul>
                 </nav>
+                <div ref={menuRef} className={"absolute left-5 bottom-5"}>
+                    <nav className={`border rounded-lg bg-white shadow p-2 ${!showMenu && "hidden"}`}>
+                        {auth.user ? (
+                            <Link href={route("dashboard")}>Tableau de bord</Link>
+                        ) : (
+                            <ul>
+                                <li>
+                                    <Link href={route('login')}>Se connecter</Link>
+                                </li>
+                                <li>
+                                    <Link href={route('register')}>Créer un compte</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </nav>
+                    <Button variant={"secondary"} onClick={() => {
+                        setShowMenu(!showMenu)
+                    }}>Profil</Button>
+                </div>
             </div>
-            <div className={"border-x border-gray-300 min-h-screen"}>
-                {article.description}
+            <div className={"md:border-x border-gray-300 min-h-screen px-5 lg:px-20 pb-20"}>
+                <h1>Résumé</h1>
+                <p className={"whitespace-pre-line"}>
+                    {article.description}
+                </p>
             </div>
             <div className={"hidden lg:flex flex-col items-center justify-center p-5 gap-10"}>
                 <h1>{article.title}</h1>
